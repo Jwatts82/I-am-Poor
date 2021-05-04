5a77123d2d6099a81d547bafc213eba363ff8d28
jwatts82 <wattscodes@gmail.com>
2021-05-04 10:20:39 -0700

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Jessica Watts on 5/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
